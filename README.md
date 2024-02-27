1ee54c450578f9b43fbf0c8a4da5fb9946ee551b
Carly <carlycarswell@gmail.com>
2024-02-27 14:32:24 +0000

    Require user auth for creating notes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Full Stack Open Part 3 Notes App
 
 BROKEN:
-note creation, need to add user field to frontend
+frontend note creation, need to add user field to frontend
 tests broken by mongoose populate method
--- controllers/notes.js
+++ controllers/notes.js
@@ -1,7 +1,19 @@
 /* eslint-disable prefer-destructuring */
+const jwt = require('jsonwebtoken');
+
 const notesRouter = require('express').Router();
+
 const Note = require('../models/note');
 const User = require('../models/user');
+
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization');
+
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '');
+  }
+  return null;
+};
 
 notesRouter.get('/', async (request, response) => {
   const notes = await Note.find({}).populate('user', { username: 1, name: 1 });
@@ -26,7 +38,11 @@ notesRouter.delete('/:id', async (request, response) => {
 notesRouter.post('/', async (request, response) => {
   const body = request.body;
 
-  const user = await User.findById(body.userId);
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'invalid token' });
+  }
+  const user = await User.findById(decodedToken.id);
 
   const note = new Note({
     content: body.content,
